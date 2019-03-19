d50311dbcdd74d6ec1078cbf4a0315aaa1d9ced3
Skye Leahy <skyeleahy@protonmail.com>
2019-03-18 21:31:54 -0700

    add delete education & experience routes

--- validation/register.js
+++ validation/register.js
@@ -33,12 +33,12 @@ module.exports = function validateRegisterInput(data) {
     errors.password = "Password must be at least 6 characters";
   }
 
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = "Confirm Password field is required";
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
   }
 
-  if (Validator.equals(data.password, data.password2)) {
-    errors.password2 = "Passwords must match";
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm Password field is required";
   }
 
   return {
